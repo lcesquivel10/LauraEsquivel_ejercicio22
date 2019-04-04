e10a775d160f7e1106e500c2337bedf131e4c5b6
lcesquivel10 <48950395+lcesquivel10@users.noreply.github.com>
2019-04-04 07:44:44 -0500

    Update and rename ejercicio22 to metropolis.cpp

---
+++ metropolis.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+
+using std::cout;
+using std::endl;
+
+N=10000;
+
+void MCMD(int N);
+
+float norm (float n);
+int main(){
+    
+  srand48(time(48)); 
+for(i=0;i<100;i++){
+    apuesta(100, &maximo, &tiempo, i);
+    cout << maximo << " " << tiempo << endl;
+  }
+
